342d1018d2fb256620a99ed163d3faf473febf0c
峻維 郭 <guoleo2008@yahoo.com.tw>
2019-07-09 22:05:31 +0800

    設定背景顏色

--- src/component/Main.vue
+++ src/component/Main.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-app>
-    <v-container>
-      <v-layout>
-        <v-flex>
-          hello world
+  <v-app class="width-100">
+    <v-container padding-0 style="min-width: 100%; max-width: 100%">
+      <v-layout row wrap height-100 padding-0>
+        <v-flex xs8 left-side height-100>
+        </v-flex>
+        <v-flex xs4 right-side height-100>
+          
         </v-flex>
       </v-layout>
     </v-container>
@@ -25,4 +27,19 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.height-100{
+  height: 100%;
+}
+.left-side{
+  background-color: #FFEDF7;
+}
+.right-side{
+  background-color: #003164;
+}
+.width-100{
+  width: 100%;
+}
+.padding-0{
+  padding: 0;
+}
 </style>
